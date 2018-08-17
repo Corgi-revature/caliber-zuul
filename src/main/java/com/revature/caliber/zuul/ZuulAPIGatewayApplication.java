e4ffd159586d7884fce3d6ff80fadb0a2deab547
GabeGallagher <zeinfandel@yahoo.com>
2018-08-17 14:39:01 -0400

    Added pre-filter to ZuulAPIGatewayApplication

--- src/main/java/com/revature/caliber/zuul/ZuulAPIGatewayApplication.java
+++ src/main/java/com/revature/caliber/zuul/ZuulAPIGatewayApplication.java
@@ -5,14 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.revature.caliber.zuul.filter.PreZuulFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulAPIGatewayApplication {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception
+	{
 		SpringApplication.run(ZuulAPIGatewayApplication.class, args);
 	}
-	
+	@Bean //added filter
+	public PreZuulFilter filer()
+	{
+		return new PreZuulFilter();
+	}
 }
